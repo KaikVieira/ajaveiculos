756a9292c03e23783098a503ba2494e45ed8a3e3
KaikVieira <vkaik0504@gmail.com>
2025-04-18 01:45:02 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,20 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+import cloudinary
+from urllib.parse import urlparse
 
 db = SQLAlchemy()
+
+def configure_cloudinary():
+    cloudinary_url = os.getenv('CLOUDINARY_URL')
+    parsed_url = urlparse(cloudinary_url)
+
+    cloudinary.config(
+        cloud_name=parsed_url.hostname,
+        api_key=parsed_url.username,
+        api_secret=parsed_url.password
+    )
 
 def create_app():
     load_dotenv()
@@ -12,7 +24,7 @@ def create_app():
 
     app.secret_key = os.environ.get('SECRET_KEY', 'sua_chave_secreta_aqui')
     base_dir = os.path.abspath(os.path.dirname(__file__))
-
+    
     database_path = os.environ.get('DATABASE_URL', None)
     if database_path is None:
         database_path = f'sqlite:///{os.path.join(base_dir, "instance", "carros.db")}'
@@ -24,7 +36,7 @@ def create_app():
 
     app.config['UPLOAD_FOLDER'] = os.path.join(base_dir, 'static', 'uploads')
     app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  
 
     app.config['USUARIO_ADMIN'] = os.getenv('USUARIO_ADMIN')
     app.config['SENHA_ADMIN'] = os.getenv('SENHA_ADMIN')
@@ -32,6 +44,8 @@ def create_app():
 
     os.makedirs(os.path.join(base_dir, 'instance'), exist_ok=True)
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+    configure_cloudinary()
 
     db.init_app(app)
 
