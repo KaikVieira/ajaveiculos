cfac0ff785b872d02b910e9a305b4017ab8f63d2
KaikVieira <vkaik0504@gmail.com>
2025-04-19 16:16:47 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -16,33 +16,33 @@ class Carro(db.Model):
         'ImagemCarro',
         backref='carro',
         lazy=True,
-        cascade='all, delete-orphan' 
+        cascade='all, delete-orphan'
     )
 
     @validates('ano')
     def validate_ano(self, key, ano):
-        """Valida o ano do carro, garantindo que ele esteja entre 1900 e o ano atual."""
+       
         current_year = datetime.now().year + 1
         if not 1900 <= int(ano) <= current_year:
             raise ValueError("Ano inválido")
         return ano
 
     def __repr__(self):
-        """Representação em string para exibir informações do carro."""
         return f'<Carro {self.nome} | {self.marca} {self.modelo} | {self.ano}>'
 
 class ImagemCarro(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url_imagem = db.Column(db.String(200), nullable=False, unique=True)
+    url_imagem = db.Column(db.String(500), nullable=False, unique=True)
     carro_id = db.Column(db.Integer, db.ForeignKey('carro.id', ondelete='CASCADE'), nullable=False)
 
     @validates('url_imagem')
     def validate_url_imagem(self, key, url_imagem):
-        """Valida a URL da imagem, garantindo que seja uma extensão permitida."""
-        if not url_imagem.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+     
+        extensoes_permitidas = ['.png', '.jpg', '.jpeg', '.gif', '.webp']
+        if not any(ext in url_imagem.lower() for ext in extensoes_permitidas):
             raise ValueError("Formato de imagem inválido")
         return url_imagem
 
     def __repr__(self):
-        """Representação em string para exibir informações da imagem."""
         return f'<ImagemCarro {self.url_imagem}>'
+
